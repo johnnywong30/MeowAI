a4d7f2ba6eefe8df9c9af66d268698d548ec1873
Johnny Wong <johnny.wong2324@gmail.com>
2023-03-23 15:08:08 -0400

    chat and history of chat

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 import os
 import openai
@@ -9,6 +9,38 @@ app = Flask(__name__)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
+
+@app.route("/history", methods=['GET'])
+def history():
+    return jsonify(chat_history), 200
+
+@app.route("/chat", methods=['POST'])
+def chat():
+    '''
+    request should be in the form of
+    {
+        user_message: <USER MESSAGE>
+    }
+    '''
+    req = request.get_json()
+    model="gpt-3.5-turbo"
+    msg = req['user_message'].strip()
+    print(f'User message: {msg}')
+    
+    chat_history.append({"role": "user", "content": msg},)
+    chat_response = openai.ChatCompletion.create(
+        model=model,
+        messages=chat_history
+    )
+    reply = chat_response['choices'][0]['message']
+    chat_history.append(reply)
+    response = {
+        "chat_msg": reply['content']
+    }
+    return jsonify(response), 200
+
+
 @app.route("/")
 def hello_world():
     return f'<p>hi</p>'
@@ -17,4 +49,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    app.run()
+    app.run(host='localhost', port=8000)
