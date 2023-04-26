f602e25d9eb902137ec75ff1c275ed6aeb057511
Johnny Wong <johnny.wong2324@gmail.com>
2023-04-26 13:19:21 -0400

    server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,9 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  },
-  server: {
+    port: 5173,
     proxy: {
       '/chat': {
         target: 'http://localhost:8000',
